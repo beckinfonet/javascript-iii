4fa5ce7ee7680268dc8ebf16fc51224115f92a2b
Bakytbek Tatibekov <BeckTJ@Bakytbeks-MacBook-Pro.local>
2017-11-29 21:46:37 -0600

    Completed!

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -7,23 +7,23 @@
 
 // In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for(var key in values) {
+  console.log(values[key])
+}
 
-// In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
+//In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+  console.log(key)
+}
 
 
 // ========================
@@ -33,51 +33,82 @@
 
 function showValues( obj ) {
   // CODE HERE
+  let output = '';
+  for ( var key in obj){
+      output += obj[key];
+  }
+  return output;
 }
 
 
 // ========================
 
 
-// Write a function called greaterThan10 that takes in an object. Write a for in loop that loops over the object and changes any value that is great than 10 to 0. Return the updated object.
+// Write a function called greaterThan10 that takes in an object. Write a for in loop that loops over the object and 
+// changes any value that is great than 10 to 0. Return the updated object.
 
 // CODE HERE
-
+const greaterThan10 = function(obj){
+  for ( var prop in obj){
+    if ( obj[prop] > 10){
+      obj[prop] = 0;
+    }
+  }
+  return obj;
+}
 
 
 // ========================
 
 
-// Write a function called double that takes in an object. Write a for in loop that loops over the object and changes every value to be itself multipled by 2. Return the updated object.
+// Write a function called double that takes in an object. Write a for in loop that loops over the object and 
+// changes every value to be itself multipled by 2. Return the updated object.
 
 // CODE HERE
-
+const double = function(obj){
+  for ( var key in obj){
+    obj[key] = obj[key] * 2;
+  }
+  return obj;
+}
 
 
 // ========================
 
 
-// Write a function called secrets that will take in an object. Create an empty string variable. Write a for in loop that loops over the object. If the property name starts with an 'sh', concatenate the value to the string variable. By the end of the for in loop, you should have a sentence, return that sentence.
+// Write a function called secrets that will take in an object. Create an empty string variable. 
+// Write a for in loop that loops over the object. If the property name starts with an 'sh', concatenate the value to the string variable. 
+// By the end of the for in loop, you should have a sentence, return that sentence.
 
 // CODE HERE
+const secrets = function(obj){
+  let data = '';
 
+  for( var key in obj){
+    if(key.startsWith('sh')=== true) {
+        data += obj[key];
+    }
+  }
+  return data;
+}
 
 // ========================
 
 
-// Sometimes it's needed to delete object properties. All you need is the word delete before a reference to the object property value. Uncomment the example below to see a for in loop deleting all the properties inside an object.
+// Sometimes it's needed to delete object properties. All you need is the word delete before a reference to the object property value. 
+// Uncomment the example below to see a for in loop deleting all the properties inside an object.
 
-// var deleteAllThethings = {
-//   one: 1,
-//   two: 2,
-//   three: 3
-// }
+var deleteAllThethings = {
+  one: 1,
+  two: 2,
+  three: 3
+}
 
-// for(var key in deleteAllThethings) {
-//   delete deleteAllThethings[key]
-// }
+for(var key in deleteAllThethings) {
+  delete deleteAllThethings[key]
+}
 
-// console.log(deleteAllThethings)
+console.log(deleteAllThethings)
 
 // ========================
 
@@ -85,7 +116,10 @@ function showValues( obj ) {
 // Write a function called removePassword that takes in an object. Delete the property password and return the object.
 
 // CODE HERE
-
+const removePassword = function(obj){
+  delete obj.password;
+  return obj;
+}
 
 
 // ========================
@@ -102,20 +136,41 @@ var deleteTheBigNumbers = {
 
 // CODE HERE
 
-
+for( var key in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[key] > 100){
+    delete deleteTheBigNumbers[key];
+  }
+}
 // ========================
 
 
-// Write a function called startsWithK that takes an object as a parameter. Write a for in loop to loop over the object. If any property name starts with k, delete that property. Return the updated object.
+// Write a function called startsWithK that takes an object as a parameter. Write a for in loop to loop over the object. 
+// If any property name starts with k, delete that property. Return the updated object.
 
 // CODE HERE
-
+const startsWithK = function(obj){
+  for ( var key in obj){
+    if(key.startsWith('k')){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 // ========================
 
 
-// Write a function called hiddenTreasure that takes in an object. Write a for in loop that loops over this object. Each property will have a sentence as it's value. If the property value does not contain the word 'treasure', delete the property. Return the updated object.
+// Write a function called hiddenTreasure that takes in an object. Write a for in loop that loops over this object. 
+// Each property will have a sentence as it's value. If the property value does not contain the word 'treasure', delete the property. Return the updated object.
 // (hint: the method includes() may be of use...)
 
 // CODE HERE
+const hiddenTreasure = function(obj){
+  for ( var key in obj){
+    if(!obj[key].includes('treasure')){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
